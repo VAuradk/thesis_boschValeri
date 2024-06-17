375d749d0ee7dc56d2f5517927f4fc44cb825fa4
VAuradk <valeri.boschcastella@gmail.com>
2024-06-18 01:37:04 +0200

    Now socket distinguishes between enemies and only one type of enemy can be accepted

--- Assets/Scripts/InGame/Socket.cs
+++ Assets/Scripts/InGame/Socket.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class Socket : MonoBehaviour
 {
+    public bool greenEnemy = false;
+    public bool yellowEnemy = false;
+    public bool blueEnemy = false;
+    public bool orangeEnemy = false;
+
     [HideInInspector] public bool isEmpty = true;
     private Vector2 socketPosition;
     [SerializeField] private float threshold = 0.1f;
 
     private void Start()
     {
-        socketPosition = transform.position;
+        socketPosition = (Vector2)transform.position;
+        IdentifyActiveEnemy();
     }
 
     private void Update()
@@ -18,19 +24,67 @@ public class Socket : MonoBehaviour
             return;
         }
 
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemyBasic");
+        GameObject[] allGameObjects = FindObjectsOfType<GameObject>();
 
-        foreach (GameObject enemy in enemies)
+        foreach (GameObject obj in allGameObjects)
         {
-            if (Vector2.Distance(socketPosition, enemy.transform.position) < threshold)
+            if (IsActiveEnemy(obj.tag))
             {
-                isEmpty = false;
-                Rigidbody2D enemyRigidbody = enemy.GetComponent<Rigidbody2D>();
-                enemyRigidbody.simulated = false;
-                enemy.transform.position = socketPosition;
-                SpriteRenderer enemyColor = enemy.GetComponent<SpriteRenderer>();
-                enemyColor.color = new Color(255f, 255f, 255f, 0.5f);
+                if (Vector2.Distance(socketPosition, obj.transform.position) < threshold)
+                {
+                    isEmpty = false;
+                    Rigidbody2D enemyRigidbody = obj.GetComponent<Rigidbody2D>();
+                    enemyRigidbody.simulated = false;
+                    obj.transform.position = socketPosition;
+                    SpriteRenderer enemyColor = obj.GetComponent<SpriteRenderer>();
+                    enemyColor.color = new Color(255f, 255f, 255f, 0.5f);
+                }
             }
         }
+    }
+
+    void IdentifyActiveEnemy()
+    {
+        if (greenEnemy)
+        {
+            Debug.Log("Green enemy is active.");
+        }
+        else if (yellowEnemy)
+        {
+            Debug.Log("Yellow enemy is active.");
+        }
+        else if (blueEnemy)
+        {
+            Debug.Log("Blue enemy is active.");
+        }
+        else if (orangeEnemy)
+        {
+            Debug.Log("Orange enemy is active.");
+        }
+        else
+        {
+            Debug.LogWarning("No enemy is active.");
+        }
+    }
+
+    bool IsActiveEnemy(string tag)
+    {
+        if (greenEnemy && tag == "enemyBasic")
+        {
+            return true;
+        }
+        if (yellowEnemy && tag == "enemyChase")
+        {
+            return true;
+        }
+        if (blueEnemy && tag == "enemyInvisible")
+        {
+            return true;
+        }
+        if (orangeEnemy && tag == "enemyRandom")
+        {
+            return true;
+        }
+        return false;
     }
 }
